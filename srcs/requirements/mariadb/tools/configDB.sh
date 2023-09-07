8730dabbf360b251c47bf0d30288277283943105
hboissel <hboissel@student.42.fr>
2023-09-07 18:08:55 +0000

    debug again

--- srcs/requirements/mariadb/tools/configDB.sh
+++ srcs/requirements/mariadb/tools/configDB.sh
@@ -26,7 +26,6 @@ mysql -uroot -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWOR
 
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; FLUSH PRIVILEGES;"
 
-#mysql -uroot -p$SQL_ROOT_PASSWORD $SQL_DATABASE < /usr/local/bin/wordpress.sql
 fi
 
 /etc/init.d/mysql stop
